fda6d6e56ce7c50a7f5b1c3c4ee2f3e9e3a95e5a
CoreNion <coreinion@gmail.com>
2024-01-11 15:05:46 +0900

    Google Analytics / Search Console用の処理を追加

--- app.vue
+++ app.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
+const { grantConsent, revokeConsent } = useGtag();
+const runtimeConfig = useRuntimeConfig();
+const { cookiesEnabledIds } = useCookieControl()
+
 useHead({
   htmlAttrs: {
     lang: 'ja',
   },
 });
+useSeoMeta({
+  googleSiteVerification: runtimeConfig.public.googleSiteVerification,
+});
+
+// Cookieの有効/無効の変更を監視
+watch(
+  () => cookiesEnabledIds.value,
+  (current, previous) => {
+    if (!previous?.includes('ga') && current?.includes('ga')) {
+      grantConsent();
+    } else if (previous?.includes('ga') && !current?.includes('ga')) {
+      revokeConsent();
+    }
+  },
+  { deep: true },
+)
 </script>
 
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <CookieControl locale="ja" />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,42 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true
   },
-  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      googleSiteVerification: '',
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-gtag', '@dargmuesli/nuxt-cookie-control', '@nuxtjs/sitemap'],
+  gtag: {
+    initialConsent: false
+  },
+  cookieControl: {
+    barPosition: 'bottom-right',
+    locales: ['en', 'ja'],
+    localeTexts: {
+      ja: {
+        bannerTitle: '解析データの収集について',
+        bannerDescription: '当サイトでは、使用状況のデータの収集のためにCookieなどを使用することがあります。このデータは匿名で収集されており、個人を特定するものではありません。データの収集に同意される場合は、「同意」ボタンを押してください。',
+        acceptAll: '同意',
+        decline: '拒否',
+        manageCookies: 'データの管理',
+      }
+    },
+    cookies: {
+      necessary: [],
+      optional: [
+        {
+          name: 'Google Analytics',
+          id: 'ga',
+          description: {
+            en: 'Google Analytics is a web analytics service offered by Google that tracks and reports website traffic.',
+            ja: 'Google Analytics は Google が提供するウェブ解析サービスで、ウェブサイトのトラフィックを追跡・レポートします。',
+          },
+          links: {
+            '/privacy.html': 'Privacy Policy',
+          }
+        }
+      ]
+    }
+  },
 })
