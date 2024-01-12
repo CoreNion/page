3dd14176f2bb0b72189b10754149cea07adcda49
CoreNion <coreinion@gmail.com>
2024-01-12 15:22:15 +0900

    [Index] 実績ウィンドウのサイズ調整

--- components/index/arch.vue
+++ components/index/arch.vue
@@ -15,11 +15,11 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <div class="mockup-window shadow-xl max-w-xl border-2 m-3 bg-green-50">
+  <div class="mockup-window shadow-xl w-[26em] 2xl:w-[30em] border-2 m-3 bg-green-50">
     <div class="card-body">
       <div class="card-title">
         <img class="rounded-xl h-16 border md:mr-2" :src="useRuntimeConfig().app.baseURL + props.icon" :alt="props.title + '\'s icon'" />
-        <h2 class="text-2xl md:text-3xl md:font-medium"> {{ props.title }}
+        <h2 class="text-3xl"> {{ props.title }}
           <div v-if="props.newBadge" class="badge badge-lg badge-accent font-medium p-1 px-3 ml-3">NEW</div>
         </h2>
       </div>
