79e09960f12c6fa6a2ddace2bda5834a83f6a6ec
CoreNion <coreinion@gmail.com>
2024-01-23 14:22:46 +0900

    [layout] ページが画面いっぱいになるように修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,61 @@
 <template>
-  <div class="navbar bg-slate-300">
-    <div class="flex-none">
-      <div class="dropdown sm:hidden">
-        <label class="btn btn-ghost btn-circle" tabindex="0">
-          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
-          </svg>
-        </label>
+  <div class="min-h-screen grid grid-rows-[auto_1fr]">
+    <div class="navbar bg-slate-300">
+      <div class="flex-none">
+        <div class="dropdown sm:hidden">
+          <label class="btn btn-ghost btn-circle" tabindex="0">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
+            </svg>
+          </label>
 
-        <ul class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52" tabindex="0">
-          <li><NuxtLink to="/trucker/" no-prefetch>FileTrucker</NuxtLink></li>
-          <li><NuxtLink to="/archivtter/" no-prefetch>Archivtter</NuxtLink></li>
-          <li><NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink></li>
-        </ul>
+          <ul class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52" tabindex="0">
+            <li>
+              <NuxtLink to="/trucker/" no-prefetch>FileTrucker</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/archivtter/" no-prefetch>Archivtter</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex-1">
+        <NuxtLink class="btn btn-ghost normal-case text-xl" to="/" no-prefetch>CoreNion</NuxtLink>
+      </div>
+      <div class="max-sm:hidden ul menu menu-horizontal justify-end px-1">
+        <li>
+          <NuxtLink to="/trucker/" no-prefetch>FileTrucker</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/archivtter/" no-prefetch>Archivtter</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink>
+        </li>
       </div>
     </div>
-    <div class="flex-1"><NuxtLink class="btn btn-ghost normal-case text-xl" to="/" no-prefetch>CoreNion</NuxtLink></div>
-    <div class="max-sm:hidden ul menu menu-horizontal justify-end px-1">
-      <li><NuxtLink to="/trucker/" no-prefetch>FileTrucker</NuxtLink></li>
-      <li><NuxtLink to="/archivtter/" no-prefetch>Archivtter</NuxtLink></li>
-      <li><NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink></li>
-    </div>
+
+    <slot></slot>
+
+    <footer class="footer items-center p-4 bg-black text-neutral-content ">
+      <aside class="items-center grid-flow-col">
+        <p>
+          <span>Copyright © 2024 CoreNion</span>
+          <br>
+          <NuxtLink class="link text-white mr-2" to="/privacy">Site's Privacy Policy</NuxtLink>
+        </p>
+      </aside>
+      <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a href="https://github.com/CoreNion">
+          <img class="h-9" src="../assets/sns/github.svg">
+        </a>
+        <a href="https://twitter.com/CoreiNion">
+          <img class="h-9" src="../assets/sns/x.svg">
+        </a>
+      </nav>
+    </footer>
   </div>
-
-  <slot></slot>
-
-  <footer class="footer items-center p-4 bg-black text-neutral-content border-t-2 pt-5">
-    <div class="items-center grid-flow-col flex flex-col">
-      <p class="text-blue-200">
-        Copyright © 2024 CoreNion
-      </p>
-      <NuxtLink class="link text-white" to="/privacy">
-        Site's Privacy Policy
-      </NuxtLink>
-    </div>
-    <div class="grid-flow-col gap-4 place-self-center justify-self-end items-center">
-      <span class="font-bold text-blue-200">Check my accounts!:</span>
-      <a href="https://github.com/CoreNion">
-        <img class="h-9" src="../assets/sns/x.svg"></a>
-        <a href="https://twitter.com/CoreiNion"><img class="h-9" src="../assets/sns/github.svg"></a>
-    </div>
-  </footer>
 </template>
--- pages/archivtter/index.vue
+++ pages/archivtter/index.vue
@@ -36,44 +36,42 @@ const searchUrl = computed(() => {
 </script>
 
 <template>
-  <div class="min-h-[83vh]">
-    <div class="my-5 mx-3 flex flex-col lg:items-center">
-      <h1 class="text-4xl text-center font-bold">Archivtter</h1>
-      <p class="text-center">
-        <span>過去のX ポスト(ツイート)を、日付の範囲で簡単に検索できるリンクを3 STEPで発行します！</span>
-      </p>
+  <div class="my-5 mx-3 flex flex-col lg:items-center">
+    <h1 class="text-4xl text-center font-bold">Archivtter</h1>
+    <p class="text-center">
+      <span>過去のX ポスト(ツイート)を、日付の範囲で簡単に検索できるリンクを3 STEPで発行します！</span>
+    </p>
 
-      <div class="flex flex-row flex-wrap gap-3 my-4">
-        <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
-          <h2 class="text-xl text-center font-bold">Step 1: ユーザー名を入力</h2>
-          <p>検索したいユーザーの、@から始まるユーザー名を入力してください。</p>
-          <div class="mt-2 join w-full">
-            <div class="join-item p-3 border bg-neutral text-neutral-content">@</div>
-            <input type="text" v-model="accountName" class="join-item input w-full" placeholder="ユーザー名" required>
-          </div>
-        </div>
-        <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
-          <h2 class="text-xl text-center font-bold">Step 2: 検索する期間の開始日を設定</h2>
-          <p>検索する日付の範囲の、開始日を入力してください。</p>
-          <input class="mt-2 input w-full" v-model="sinceDate" type="date" placeholder="ブラウザーの機能を利用するか、yyyy-mm-ddの形式で入力"
-            required>
-        </div>
-        <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
-          <h2 class="text-xl text-center font-bold">Step 3: 検索する期間の終了日を設定</h2>
-          <p>検索する日付の範囲の、終了日を入力してください。</p>
-          <input class="mt-2 input w-full" v-model="untilDate" type="date" placeholder="ブラウザーの機能を利用するか、yyyy-mm-ddの形式で入力"
-            required>
+    <div class="flex flex-row flex-wrap gap-3 my-4">
+      <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
+        <h2 class="text-xl text-center font-bold">Step 1: ユーザー名を入力</h2>
+        <p>検索したいユーザーの、@から始まるユーザー名を入力してください。</p>
+        <div class="mt-2 join w-full">
+          <div class="join-item p-3 border bg-neutral text-neutral-content">@</div>
+          <input type="text" v-model="accountName" class="join-item input w-full" placeholder="ユーザー名" required>
         </div>
       </div>
-
-      <div class="divider"></div>
-
-      <div class="border-2 rounded-xl p-5 max-w-screen-lg">
-        <h1 class="text-xl font-bold">作成結果</h1>
-
-        <div class="whitespace-nowrap overflow-x-auto font-mono my-2">{{ searchUrl }}</div>
-        <a class="btn w-full" :href="searchUrl">検索結果にアクセス</a>
+      <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
+        <h2 class="text-xl text-center font-bold">Step 2: 検索する期間の開始日を設定</h2>
+        <p>検索する日付の範囲の、開始日を入力してください。</p>
+        <input class="mt-2 input w-full" v-model="sinceDate" type="date" placeholder="ブラウザーの機能を利用するか、yyyy-mm-ddの形式で入力"
+          required>
       </div>
+      <div class="grow w-96 bg-base-200 text-base-content p-4 rounded-xl">
+        <h2 class="text-xl text-center font-bold">Step 3: 検索する期間の終了日を設定</h2>
+        <p>検索する日付の範囲の、終了日を入力してください。</p>
+        <input class="mt-2 input w-full" v-model="untilDate" type="date" placeholder="ブラウザーの機能を利用するか、yyyy-mm-ddの形式で入力"
+          required>
+      </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="border-2 rounded-xl p-5 max-w-screen-lg">
+      <h1 class="text-xl font-bold">作成結果</h1>
+
+      <div class="whitespace-nowrap overflow-x-auto font-mono my-2">{{ searchUrl }}</div>
+      <a class="btn w-full" :href="searchUrl">検索結果にアクセス</a>
     </div>
   </div>
 </template>
