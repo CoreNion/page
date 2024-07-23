9ef5b88c66d1fb89b29d521b508358aeee54eced
CoreNion <coreinion@gmail.com>
2024-07-23 15:45:58 +0900

    依存関係の更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     typeCheck: true
   },
+
   app: {
     "baseURL": process.env.BASE_URL || "/",
     pageTransition: { name: 'page', mode: 'out-in' },
   },
+
   vue: {
     compilerOptions: {
       isCustomElement: tag => tag === 'ms-store-badge'
     }
   },
+
   runtimeConfig: {
     public: {
       googleSiteVerification: '',
       mailAdress: "hoge@example.com", 
     }
   },
-  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon', 'nuxt-gtag', '@nuxtjs/sitemap'],
+
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt-gtag',
+    '@nuxtjs/sitemap',
+    "@nuxt/icon"
+  ],
+
   gtag: {
     enabled: false
   },
+
+  compatibilityDate: '2024-07-23',
 })
