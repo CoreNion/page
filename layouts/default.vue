012c37349138870c7a0c2b02b134b90d610366c0
CoreNion <coreinion@gmail.com>
2024-11-11 17:25:57 +0900

    [layout] より合理的なレイアウト構成にする

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,13 @@ const enableGA = enableGoogleAnalytics();
 </script>
 
 <template>
-  <div class="min-h-screen grid grid-rows-[auto_1fr]">
-    <div class="navbar bg-slate-300">
+  <div class="min-h-screen grid grid-rows-[auto_1fr_auto]">
+    <header class="navbar bg-slate-300">
       <div class="flex-none">
-        <div class="dropdown sm:hidden">
+        <nav class="dropdown sm:hidden">
           <label class="btn btn-ghost btn-circle" tabindex="0">
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
             </svg>
           </label>
@@ -24,12 +25,12 @@ const enableGA = enableGoogleAnalytics();
               <NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink>
             </li>
           </ul>
-        </div>
+        </nav>
       </div>
       <div class="flex-1">
         <NuxtLink class="btn btn-ghost normal-case text-xl" to="/" no-prefetch>CoreNion</NuxtLink>
       </div>
-      <div class="max-sm:hidden ul menu menu-horizontal justify-end px-1">
+      <ul class="max-sm:hidden menu menu-horizontal justify-end px-1">
         <li>
           <NuxtLink to="/trucker/" no-prefetch>FileTrucker</NuxtLink>
         </li>
@@ -39,31 +40,29 @@ const enableGA = enableGoogleAnalytics();
         <li>
           <NuxtLink to="/leasy/" no-prefetch>Leasy</NuxtLink>
         </li>
-      </div>
-    </div>
+      </ul>
+    </header>
 
     <slot></slot>
 
-    <footer class="footer items-center p-4 bg-black text-neutral-content ">
-      <div class="w-full flex flex-row justify-stretch">
-        <p class="flex-1">
-          <span>© 2024 CoreNion</span>
-          <br>
-          <NuxtLink class="link text-white mr-2" to="/privacy">Privacy</NuxtLink>
-          <a class="link text-blue-200" href="https://github.com/CoreNion/page">Source Code</a>
-        </p>
-        <p class="flex-1 flex flex-row justify-end items-center gap-4">
-          <a href="https://github.com/CoreNion">
-            <Icon name="carbon:logo-github" size="40px"></Icon>
-          </a>
-          <a href="https://twitter.com/CoreiNion">
-            <Icon name="carbon:logo-x" size="40px"></Icon>
-          </a>
+    <footer class="p-4 bg-black text-neutral-content grid grid-cols-[1fr_1fr]">
+      <div>
+        <span>© 2024 CoreNion</span>
+        <br>
+        <NuxtLink class="link text-white mr-2" to="/privacy">Privacy</NuxtLink>
+        <a class="link text-blue-200" href="https://github.com/CoreNion/page">Source Code</a>
+      </div>
+      <div class="grid grid-cols-[repeat(3,auto)] justify-end items-center gap-4">
+        <a href="https://github.com/CoreNion">
+          <Icon name="carbon:logo-github" size="40px"></Icon>
+        </a>
+        <a href="https://twitter.com/CoreiNion">
+          <Icon name="carbon:logo-x" size="40px"></Icon>
+        </a>
 
-          <button class="btn btn-primary" onclick="analyticsModal.showModal()">
-            <Icon name="carbon:data-analytics" size="25px"></Icon>
-          </button>
-        </p>
+        <button class="btn btn-primary" onclick="analyticsModal.showModal()">
+          <Icon name="carbon:data-analytics" size="25px"></Icon>
+        </button>
       </div>
     </footer>
 
