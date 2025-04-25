1b133d4c5e0b903d40bdd65153ac135a88e31443
CoreNion <coreinion@gmail.com>
2025-04-25 14:25:24 +0900

    [layout] footerを実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,28 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <footer className="p-4 bg-black text-white text-neutral-content grid grid-cols-[1fr_1fr]">
+          <div>
+            <span>© 2025 CoreNion</span>
+            <br />
+            <Link className="underline text-white mr-2" href="/privacy" target="_blank">Privacy</Link>
+            <Link className="underline text-blue-200" href="https://github.com/CoreNion/page" target="_blank">Source Code</Link>
+          </div>
+
+          <nav className="grid grid-cols-[repeat(3,auto)] justify-end items-center gap-4">
+            <Link href="https://github.com/CoreNion">
+              <Icon icon="carbon:logo-github" height="40px"></Icon>
+            </Link>
+            <Link href="https://x.com/CoreiNion">
+              <Icon icon="carbon:logo-x" height="40px"></Icon>
+            </Link>
+
+            <button className="bg-yellow-100 text-black rounded-full p-2">
+              <Icon icon="carbon:data-analytics" height="25px"></Icon>
+            </button>
+          </nav>
+        </footer>
       </body>
     </html>
   );
