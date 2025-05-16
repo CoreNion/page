4a6b3af572ceaf829ddf6504f6d75b6083184fab
CoreNion <coreinion@gmail.com>
2025-05-16 10:36:02 +0900

    [components] 細かい実装を変更

--- components/app/feature.tsx
+++ components/app/feature.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AppFeature({ title, children }: { title: string; children: React.ReactNode }) {
   return (
     <div className="p-5 bg-white border border-gray-200 rounded-2xl shadow-md flex-1 grow min-w-[260px]">
-      <h2 className="text-2xl font-bold text-black mb-2">{title}</h2>
+      <h3 className="text-2xl font-bold text-black mb-2">{title}</h3>
       <div className="text-black text-base">{children}</div>
     </div>
   );
