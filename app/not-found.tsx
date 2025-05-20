cd7d2805f0089d8e1df8d4c48b19bbb97c28c696
CoreNion <coreinion@gmail.com>
2025-05-20 13:42:39 +0900

    404ページを実装

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export const metadata = {
+  title: '404 Not Found | CoreNion',
+}
+ 
+export default function NotFound() {
+  return (
+    <main className='grid place-content-center text-center gap-3'>
+      <h1 className='font-bold text-4xl'>404 | Not Found</h1>
+      <p>お探しのページは見つかりませんでした。</p>
+      <Link className='p-2 bg-slate-100 hover:bg-slate-200 transition-all duration-200 border border-slate-400 rounded-full' href="/">ホームへ戻る</Link>
+    </main>
+  )
+}
