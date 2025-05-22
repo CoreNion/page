376313ad42e49efbe3d0dd7546e43930b72c00a9
CoreNion <coreinion@gmail.com>
2025-05-22 18:38:06 +0900

    [layout] ページ遷移時にアニメーションを付ける

---
+++ app/template.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { AnimatePresence, motion } from 'motion/react'
+import React from 'react';
+import { usePathname } from 'next/navigation'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname()
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathName}
+        initial={{ clipPath: 'circle(0%)' }}
+        animate={{ clipPath: 'circle(150%)' }}
+        exit={{ clipPath: 'circle(0%)' }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className='grid grid-cols-[1fr]'
+        >
+        {children}
+
+      </motion.div>
+    </AnimatePresence>
+  )
+}
